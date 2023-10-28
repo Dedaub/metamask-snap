8e571f4e22721fc11ce53f8c80da7c64d1ae8ddd
Nikos Petridis <nick.petridis93@gmail.com>
2023-10-28 21:21:47 +0300

    feat: Publish snap to npm registry

--- packages/site/src/config/snap.ts
+++ packages/site/src/config/snap.ts
@@ -6,5 +6,6 @@
  * don't. Instead, rename `.env.production.dist` to `.env.production` and set the production URL
  * there. Running `yarn build` will automatically use the production environment variables.
  */
-export const defaultSnapOrigin =
-  process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
+// export const defaultSnapOrigin =
+//   process.env.SNAP_ORIGIN ?? `local:http://localhost:8080`;
+export const defaultSnapOrigin = 'npm:dedaub-metamask-snap';
